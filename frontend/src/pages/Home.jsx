ceb9e379e6289c86aaed2a89cd926108da4fa3d0
abuahassan <abu.a.hassan@gmail.com>
2022-09-30 12:25:33 +0800

    Hom, Login & Register UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom'
+import { FaQuestionCircle, FaMoneyBillAlt } from 'react-icons/fa'
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section className='heading'>
+        <h1>Record Income or Expense?</h1>
+        <p>Please choose from an option below</p>
+      </section>
+
+      <Link to='/new-voucher' className='btn btn-reverse btn-block'>
+        <FaQuestionCircle /> Create New Voucher
+      </Link>
+
+      <Link to='/vouchers' className='btn btn-block'>
+        <FaMoneyBillAlt /> View My Vouchers
+      </Link>
+    </>
   )
 }
 
