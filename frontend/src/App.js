0664c80c28bf625dd1c673f09243443ca0a25290
abuahassan <abu.a.hassan@gmail.com>
2022-10-05 16:30:50 +0800

    Frontend route guard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header'
+
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import NewVoucher from './pages/NewVoucher'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/new-voucher' element={<PrivateRoute />}>
+              <Route path='/new-voucher' element={<NewVoucher />} />
+            </Route>
           </Routes>
         </div>
       </Router>
---
+++ frontend/src/pages/NewVoucher.jsx
@@ -0,0 +1,9 @@
+function NewVoucher() {
+  return (
+    <div>
+      <h1>New Voucher</h1>
+    </div>
+  )
+}
+
+export default NewVoucher
