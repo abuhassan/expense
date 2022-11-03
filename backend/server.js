010d10efcaaa4eaa02e11abb798ec792a32ea1ba
abuahassan <abu.a.hassan@gmail.com>
2022-11-03 22:29:32 +0800

    Final commit before deploying

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleawre')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
@@ -13,11 +14,21 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'welcome to Income/Expense Vouchers records' })
-})
+//Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  //Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  )
+} else {
+  app.get('/', (req, res) => {
+    res
+      .status(200)
+      .json({ message: 'welcome to Income/Expense Vouchers records' })
+  })
+}
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
